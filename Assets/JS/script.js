49fe5dfc11df81b2657ea2e6ca59f44bfdcb4e70
Pratik <pratikpawar2703@gmail.com>
2023-01-28 14:10:07 +0530

    first commit

---
+++ Assets/JS/script.js
@@ -0,0 +1,71 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+
+
+
+//theme change
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+// Add or remove the dark / icon theme
+document.body.classList.toggle(darkTheme)
+themeButton.classList.toggle(iconTheme)
+// We save the theme and the current icon that the user chose
+localStorage.setItem('selected-theme', getCurrentTheme())
+localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+//slider
+const slides = document.querySelectorAll(".slide")
+//console.log(slides)
+var counter = 0;
+slides.forEach(
+    (slide,index) => {
+        slide.style.left = '${index * 100}%'
+    }
+)
+
+const goNext = () => {
+    counter++
+    slideImage()
+}
+
+const goPrev = () => {
+    counter--
+    slideImage()
+}
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = 'translateX(-${counter * 100}%)'
+        }
+    )
+}
